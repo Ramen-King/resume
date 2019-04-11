aad6211c95d36b9d3a12d3e6482b128f168ca53b
Jimmy Lai <jimmy.lai@codeimmersives.com>
2019-04-11 10:19:20 -0400

    added loop for init()

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
 window.onload = init;
+const secret = "over here";
+function init() {
+  console.log(aboutMe);
+  let collectionOfBlocks = document.getElementsByClassName("block");
 
-function init();
+  for (let i = 0; i < collectionOfBlocks.length; i++) {
+    collectionOfBlocks[i].addEventListener("click", selector);
+  }
+}
+function selector(event) {
+  event.preventDefault();
 
-document.querySelector('block')
-    .addEventListener('click', display);
+  const key = event.target.innerText.toLowerCase();
+  console.log(key);
+  //key is now accessible for both html and objects;
+  const sectionUl = document.querySelector(`#${key}`);
+  console.log(sectionUl);
 
-function
+  if (Array.isArray(aboutMe[key])) {
+    const newLi2 = aboutMe[key];
+    console.log(newLi2);
+    newLi2.forEach(function(data) {
+      const createLi = document.createElement("li");
+      createLi.innerText = data;
+      sectionUl.appendChild(createLi);
+    });
+    return newLi2;
+  } else {
+    const newLi = Object.values(aboutMe[key]);
+    newLi.forEach(function(data) {
+      const createLi = document.createElement("li");
+      createLi.innerText = data;
+      sectionUl.appendChild(createLi);
+    });
+    return newLi;
+  }
+}
